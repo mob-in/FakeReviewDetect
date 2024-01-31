3450f8239cf9ee8e2be8f72846db1a4f8320fa31
mob-in <110026198+mob-in@users.noreply.github.com>
2024-01-31 19:26:53 +0400

    phase 2

--- frontend.py
+++ frontend.py
@@ -2,6 +2,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 from ipynb import text_process
+import os
 def main1():
     html_temp = """
             <div style="background-color:lightblue;padding:16px">
@@ -18,6 +19,12 @@ def main1():
     #u_dataframe = pd.DataFrame(user_input)
 
     if st.button('PREDICT'):
+        model_path = os.path.abspath('best_model.sav')
+        if os.path.exists(model_path):
+            pred_model = joblib.load(model_path)
+                # Rest of your code...
+        else:
+            print(f"Model file '{model_path}' not found.")
         res = pred_model.predict(user_input )
         if res[0]=="OR":
             st.write("""The Review Is ORIGINAL""")
