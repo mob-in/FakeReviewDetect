d51971c4c5ec39131959beab9653817f84cd0879
mob-in <110026198+mob-in@users.noreply.github.com>
2024-01-31 19:37:29 +0400

    phase 2

--- ipynb.py
+++ ipynb.py
@@ -3,10 +3,11 @@ import warnings, string
 warnings.filterwarnings('ignore')
 
 from nltk.corpus import stopwords
-
-
+import nltk
+nltk.download("stopwords")
 
 def text_process(review):
+    
     nopunc = [char for char in review if char not in string.punctuation]
     nopunc = ''.join(nopunc)
     return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
